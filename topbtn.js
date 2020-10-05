e467aae3a9c83e43bfd1fa8de4fc56d3b230d12c
Dev Kumar <techdevg@gmail.com>
2020-10-05 10:43:43 +0530

    initial commit

---
+++ topbtn.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('#topbtn').fadeIn();
+        }
+        else{
+            $('#topbtn').fadeOut();
+        }
+});
+    $('#topbtn').click(function(){
+        $('html ,body').animate({scrollTop : 0},800);
+    });
+});
